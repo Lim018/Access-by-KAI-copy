c87fcf53044f30c931bfef225de23bb288bd87ae
alim <149599272+Lim018@users.noreply.github.com>
2025-05-09 01:21:24 +0700

    install appwrite

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
+import 'package:appwrite/appwrite.dart';
 
+Client client = Client();
+client
+    .setEndpoint('http://localhost:3309/v1')
+    .setProject('681cf430000b8a0e6999')
+    .setSelfSigned(status: true); // For self signed certificates, only use for development;
+  
 void main() {
   runApp(const MyApp());
 }
